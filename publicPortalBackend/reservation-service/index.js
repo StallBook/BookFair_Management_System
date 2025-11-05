57be156477d21f940a27f25ab46402ef1a4eec3f
ImashaNawodi <imashanaw1999@gmail.com>
2025-11-05 10:36:23 +0530

    add reservation service and done the folder setup

---
+++ publicPortalBackend/reservation-service/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5003;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI_RESERVATION_SERVICE)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Reservation service running on port ${PORT}`);
+});
