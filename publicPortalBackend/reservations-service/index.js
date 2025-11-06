714915cf65b21fb9b61cbfe431661c1e2336e512
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-11-06 12:30:17 +0530

    Assign 5004 port for reservation service and connected with db

---
+++ publicPortalBackend/reservations-service/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import cors from 'cors'
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(` ${req.method} ${req.url}`);
+    next();
+});
+
+
+
+mongoose.connect(process.env.RESERVATION_MONGODB_URI)
+    .then(() => console.log('Connected to Reservation MongoDB'))
+    .catch(err => console.error(err));
+
+
+const PORT = process.env.RESERVATION_PORT || 5004;
+app.listen(PORT, () => console.log(`Reservation Service running on port ${PORT}`))
