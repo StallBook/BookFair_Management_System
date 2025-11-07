a04953567c1784655e65e0e64b7580f7a1e3dfe8
Piyumi <piyumiprasadini133@gmail.com>
2025-11-08 00:35:25 +0530

    Add logo to the signup

--- employeefrontend/src/pages/Signup.tsx
+++ employeefrontend/src/pages/Signup.tsx
@@ -38,30 +38,30 @@ const Signup = () => {
   return (
     <div className="relative min-h-screen  bg-cover bg-center flex items-center justify-center">
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/30" />
+      <div className="absolute inset-0 bg-black/50" />
 
-      {/* Card */}` 1`
-      <div className="relative bg-white/40 backdrop-blur-md shadow-2xl rounded-2xl p-8 w-11/12 max-w-md text-center">
-        <Title level={2} style={{ color: "white", marginBottom: 4 }}>
+      {/* Card */}
+      <div className="relative bg-white/40  backdrop-blur-md shadow-2xl rounded-2xl p-8 w-11/12 max-w-md text-center">
+        <Title level={3} style={{ color: "black", marginBottom: 4, fontWeight: 600}}>
           Create your account
         </Title>
-        <Text style={{ color: "rgba(255,255,255,0.7)" }}>
-          Join the BookFair employee portal
+        <Text style={{ color: "rgba(16, 1, 1, 0.7)", fontWeight: 500 }}>
+          Join the  StallBook employee portal
         </Text>
 
         <div className="mt-6 space-y-4 text-left">
           <div>
-            <Text style={{ color: "white" }}>Full Name</Text>
+            <Text style={{ color: "blue" }}>Full Name</Text>
             <Input placeholder="John Doe" />
           </div>
 
           <div>
-            <Text style={{ color: "white" }}>Email</Text>
+            <Text style={{ color: "blue" }}>Email</Text>
             <Input type="email" placeholder="you@example.com" />
           </div>
 
           <div>
-            <Text style={{ color: "white" }}>Password</Text>
+            <Text style={{ color: "blue" }}>Password</Text>
             <Input.Password placeholder="••••••••" />
           </div>
 
@@ -75,12 +75,12 @@ const Signup = () => {
         </div>
 
         <div className="mt-6 text-sm">
-          <Text style={{ color: "rgba(255,255,255,0.7)" }}>
+          <Text style={{ color: "rgba(0, 0, 0, 0.7)" }}>
             Already have an account?{" "}
           </Text>
           <Button
             type="link"
-            style={{ color: "#93c5fd", padding: 0 }}
+            style={{ color: "#e4202dff", padding: 0 }}
             onClick={() => navigate("/signin")}
           >
             Sign in
