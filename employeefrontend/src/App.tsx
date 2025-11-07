05369f9ddbc0e1eb12b88874ea9c52de664fed25
Piyumi <piyumiprasadini133@gmail.com>
2025-11-07 18:21:11 +0530

    Update the route

--- employeefrontend/src/App.tsx
+++ employeefrontend/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path="/signup" element={<Signup />} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
