554eced9ff26c43a8f31c10bb698abb802e401b5
Piyumi <piyumiprasadini133@gmail.com>
2025-11-08 23:08:34 +0530

    add route

--- employeefrontend/src/routes/Routes.tsx
+++ employeefrontend/src/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route,Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
     </Routes>
   );
 };
