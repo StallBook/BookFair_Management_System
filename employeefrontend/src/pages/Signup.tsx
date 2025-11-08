46cebcc9ebd9849d5c6635386f3cf8aaed83a87c
Piyumi <piyumiprasadini133@gmail.com>
2025-11-08 08:57:09 +0530

    Add background for signup

--- employeefrontend/src/pages/Signup.tsx
+++ employeefrontend/src/pages/Signup.tsx
@@ -1,31 +1,4 @@
-// import React from "react";
-// import { Typography } from "antd";
-// import { useNavigate } from "react-router-dom";
-
-// const { Text } = Typography;
-
-
-// const Signup = () => {
-//     const navigate = useNavigate();
-
-//     return (
-//         <div
-//             className="min-h-screen bg-cover bg-center flex items-center justify-center">
-//             {/* Transparent Overlay */}
-//             <div className="absolute inset-0 bg-black/30"></div>
-
-//             {/* Signup Card */}
-//             <div className="justify-center items-center bg-white/40 backdrop-blur-md shadow-2xl rounded-2xl p-8 w-11/12 max-w-md text-center">
-
-                
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Signup;
-
-
+import bg from '../assets/bg1.png';
 import React from "react";
 import { Typography, Input, Button } from "antd";
 import { useNavigate } from "react-router-dom";
@@ -36,7 +9,10 @@ const Signup = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="relative min-h-screen  bg-cover bg-center flex items-center justify-center">
+    <div 
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+            style={{ backgroundImage: `url(${bg})` }}>
+      
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/50" />
 
